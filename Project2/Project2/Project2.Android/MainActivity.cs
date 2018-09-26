7a6d6d91347996aef9703c11584f1a1b9d75aafb
BuildTools <unconfigured@null.spigotmc.org>
2018-09-26 15:46:14 +1000

    Login Working, segueing to next page, Google authenticator, DNS not working

--- Project2/Project2/Project2.Android/MainActivity.cs
+++ Project2/Project2/Project2.Android/MainActivity.cs
@@ -12,13 +12,14 @@ namespace Project2.Droid
     [Activity(Label = "Project2", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            base.OnCreate(bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            global: Xamarin.Auth.Presenters.XamarinAndroid.AuthenticationConfiguration.Init(this, bundle);
             LoadApplication(new App());
         }
     }
--- Project2/Project2/Project2/View/LoginPage.xaml.cs
+++ Project2/Project2/Project2/View/LoginPage.xaml.cs
@@ -8,17 +8,24 @@ using Project2.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Project2.ViewModel;
+using Xamarin.Auth;
+using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace Project2.View
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginPage : ContentPage
 	{
-		public LoginPage ()
+        Account account;
+        AccountStore store;
+        public LoginPage ()
 		{
 			InitializeComponent ();
             Init();
-		}
+            store = AccountStore.Create();
+            account = store.FindAccountsForService(AppAuthenParameters.AppName).FirstOrDefault();
+        }
 
         void Init()
         {
@@ -31,20 +38,86 @@ namespace Project2.View
             Entry_Username.Completed += (s, e) => Entry_Password.Focus();
             Entry_Password.Completed += (s, e) => SignInProcedure(s, e);
         }
-        void SignInProcedure (object sender, EventArgs e)
+         async void SignInProcedure (object sender, EventArgs e)
 
         {
             Login user = new Login(Entry_Username.Text, Entry_Password.Text);
             if (user.CheckInformation())
             {
                 DisplayAlert("login", "Login Sucess", "OK");
-                App.UserDatabase.SaveUser(user);
-
+                if(Device.OS == TargetPlatform.Android)
+                {
+                    Application.Current.MainPage = new NavigationPage(new MenuPage());
+                }
+                else if(Device.OS == TargetPlatform.iOS)
+                {
+                    await Navigation.PushModalAsync(new NavigationPage(new MenuPage()));
+                }
             }
             else
             {
                 DisplayAlert("Login", "Login Not Correct, Empty", "OK");
             }
+
         }
-	}
+        void OnLoginClicked(object sender, EventArgs e)
+        {
+            string clientId = AppAuthenParameters.AndroidClientId;
+            string redirectUri = AppAuthenParameters.AndroidRedirectUrl;
+
+            var authenticator = new OAuth2Authenticator(
+                clientId,
+                null,
+                AppAuthenParameters.Scope,
+                new Uri(AppAuthenParameters.AuthorizeUrl),
+                new Uri(redirectUri),
+                new Uri(AppAuthenParameters.AccessTokenUrl),
+                null,
+                true);
+            AuthenState.Authenticator = authenticator;
+            authenticator.Completed += OnAuthCompleted;
+            authenticator.Error += OnAuthError;
+
+            var presenter = new Xamarin.Auth.Presenters.OAuthLoginPresenter();
+            presenter.Login(authenticator);
+        }
+        async void OnAuthCompleted(object sender, AuthenticatorCompletedEventArgs e)
+        {
+            var authenticator = sender as OAuth2Authenticator;
+            if (authenticator != null)
+            {
+                authenticator.Completed -= OnAuthCompleted;
+                authenticator.Error -= OnAuthError;
+            }
+
+            UserInfo user = null;
+            if (e.IsAuthenticated)
+            {
+                var request = new OAuth2Request("GET", new Uri(AppAuthenParameters.UserInfoUrl), null, e.Account);
+                var response = await request.GetResponseAsync();
+                if (response != null)
+                {
+                    string userJson = await response.GetResponseTextAsync();
+                    user = JsonConvert.DeserializeObject<UserInfo>(userJson);
+                }
+                if (account != null)
+                {
+                    store.Delete(account, AppAuthenParameters.AppName);
+                }
+                await store.SaveAsync(account = e.Account, AppAuthenParameters.AppName);
+                await DisplayAlert("Email address", user.Email, "OK");
+            }
+        }
+        void OnAuthError(object sender, AuthenticatorErrorEventArgs e)
+        {
+            var authenticator = sender as OAuth2Authenticator;
+            if (authenticator != null)
+            {
+                authenticator.Completed -= OnAuthCompleted;
+                authenticator.Error -= OnAuthError;
+            }
+
+            Debug.WriteLine("Authenticatin Error: " + e.Message);
+        }
+    }
 }
